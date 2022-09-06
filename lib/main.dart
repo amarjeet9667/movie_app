f3072bbdbdc5ea896d5a8246c675322ca264101e
amarjeet9667 <jeetstark1@gmail.com>
2022-09-06 22:04:41 +0530

    this is my commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import './screens/dashboard_screen.dart';
+import './screens/movie_details_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowMaterialGrid: false,
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      home: DashboardScreen(),
+      routes: {
+        MovieDetailsScreen.routeName: (ctx) => MovieDetailsScreen(),
+      },
+    );
+  }
+}
